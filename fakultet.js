1a45df8cb8a5ea735e21de1711b9294a1f419459
DavidDave8 <davidfossum2@gmail.com>
2025-04-01 10:34:52 +0200

    FAKULTET FIX

--- fakultet.js
+++ fakultet.js
@@ -1,7 +1,8 @@
-export function fakultet(tall) {
-    if (tall < 0) return NaN; 
-    if (tall <= 1) return 1;  
-    return tall * fakultet(tall - 1);
+export function fakultet (tall) {
+    if (tall<=0)
+        return NaN
+    if (tall <=2)
+        return tall
+    else 
+        return fakultet (tall-1)*tall
 }
-
-module.exports = { fakultet };
